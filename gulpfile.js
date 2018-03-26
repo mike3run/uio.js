5c589bb6ed3acdab3a329aad66356b2b989063c7
Miguel Palau Zarza <mpalauzarza@gmail.com>
2018-03-26 13:19:22 -0600

    adding the necessary tools

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+const gulp = require('gulp')
+const imagemin = require('gulp-imagemin')
+const imageminPngquant = require('imagemin-pngquant');
+const imageminZopfli = require('imagemin-zopfli');
+const imageminMozjpeg = require('imagemin-mozjpeg'); // need to run 'brew install libpng'
+const imageminGiflossy = require('imagemin-giflossy');
+
+gulp.task('imagemin', () =>
+  gulp.src(['src/**/*.{gif,png,jpg,jpeg,svg}'])
+    .pipe(imagemin([
+    // png
+      imageminPngquant({
+        speed: 1,
+        quality: 98 // lossy settings
+      }),
+      imageminZopfli({
+        more: true
+      }),
+      imageminGiflossy({
+        optimizationLevel: 3,
+        optimize: 3, // keep-empty: Preserve empty transparent frames
+        lossy: 2
+      }),
+      // svg
+      imagemin.svgo({
+        plugins: [
+          {
+            cleanupListOfValues: true
+          },
+          {
+            sortAttrs: true
+          }
+        ]
+      }),
+      // jpg lossless
+      imagemin.jpegtran({
+        progressive: true
+      }),
+      // jpg very light lossy, use vs jpegtran
+      imageminMozjpeg({
+        quality: 90
+      })
+    ]))
+    .pipe(gulp.dest('dist'))
+);
